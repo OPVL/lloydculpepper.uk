245bddd63c02e60020dc56625ce49d095b4c58f0
Lloyd Culpepper <lloydculpepper4@gmail.com>
2022-04-12 13:04:40 +0100

    social links updated

--- config/social.php
+++ config/social.php
@@ -9,13 +9,18 @@ return [
     [
         'label' => 'instagram',
         'icon' => 'fa fa-instagram',
-        'link' => 'https://www.instagram.com/never.not.yours/',
+        'link' => 'https://www.instagram.com/idonotconsent.fk/',
     ],
     [
-        'label' => 'reddit',
-        'icon' => 'fa fa-reddit-alien',
-        'link' => 'https://www.reddit.com/user/MilkTheSloth',
+        'label' => 'twitter',
+        'icon' => 'fa-brands fa-twitter',
+        'link' => 'https://twitter.com/opalbeats',
     ],
+    // [
+    //     'label' => 'reddit',
+    //     'icon' => 'fa fa-reddit-alien',
+    //     'link' => 'https://www.reddit.com/user/MilkTheSloth',
+    // ],
     [
         'label' => 'linkedin',
         'icon' => 'fa fa-linkedin',
@@ -25,5 +30,15 @@ return [
         'label' => 'soundcloud',
         'icon' => 'fa fa-soundcloud',
         'link' => 'https://soundcloud.com/opalbeats',
+    ],
+    [
+        'label' => 'spotify',
+        'icon' => 'fa-brands fa-spotify',
+        'link' => 'https://open.spotify.com/artist/6hx6U1u1OyLkgT3KVwr7Hp',
+    ],
+    [
+        'label' => 'email',
+        'icon' => 'fa-solid fa-envelope',
+        'link' => 'mailto:shout@lloydculpepper.uk',
     ],
 ];
